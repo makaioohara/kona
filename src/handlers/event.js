337e1123cdd40ca7ea1f5062c830317ba875559d
Shahriar Islam <dontleavebae@gmail.com>
2025-02-24 23:05:28 +0600

    Update event.js

--- src/handlers/event.js
+++ src/handlers/event.js
@@ -1,1 +1,8 @@
-module.exports = () => {};
+const path = require('path');
+const reusable = require('../utils/reusable');
+
+module.exports = (client) => {
+    const eventFolders = reusable(path.join(__dirname, '..', 'events'), true);
+
+    console.log(eventFolders);
+};
