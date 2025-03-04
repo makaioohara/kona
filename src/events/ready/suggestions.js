b466ad886a7b51a5fa852798b2f9988c12b05894
Shahriar Islam <dontleavebae@gmail.com>
2025-03-04 09:19:09 +0000

    Initial Commit

--- src/events/ready/suggestions.js
+++ src/events/ready/suggestions.js
@@ -4,11 +4,14 @@ const randomMessages = [
     "**Did u know?** Verified female users in the server receive the '@Baddie The Best Waifu' role to stand out and be recognized!",
     "**Did u know?** When you boost, you get a 25% rank-up boost, helping you earn XP faster and level up in no time!",
     "**Did u know?** At server level 10, you unlock the ability to use any sound on the soundboard, bringing even more fun to vc activities!",
-    "**Did u know?** Reaching Level 20 lets you change your nickname anytime, giving you the flexibility in how you're known in the server!"
+    "**Did u know?** Reaching Level 20 lets you change your nickname anytime, giving you the flexibility in how you're known in the server!",
+    "**Did u know?** We have a NSFW channel and u can get access to it just by taking the NSFW role! I'm not gonna tell u how top get this role, silly!",
+    "**Did u know?** You can come play minigames with others and make new friends here in this server!",
+    "**Did u know?** You now have to join a vc to be able to react to text messages! Don't want the hassle? Boost our server then!"
 ];
 
 module.exports = async (client) => {
-    setInterval(async () => {
+    const sendRandomMessage = async () => {
         const randomMessage = randomMessages[Math.floor(Math.random() * randomMessages.length)];
         if (!randomMessage) {
             console.error('Random message is empty.');
@@ -27,5 +30,9 @@ module.exports = async (client) => {
         } catch (err) {
             console.error('Error sending message:', err);
         }
-    }, 900000); // 15 minutes
+    };
+
+    sendRandomMessage();
+
+    setInterval(sendRandomMessage, 900000);
 };
